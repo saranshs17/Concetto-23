34569fb12aeaf30cbaefa90a6d6d8dc6c675f197
tilakraj <rajbageltilak@gmail.com>
2023-03-18 17:25:10 +0530

    home fragment completed

--- app/src/main/java/com/example/hackfestapp/ui/homefragment/HomeFragment.kt
+++ app/src/main/java/com/example/hackfestapp/ui/homefragment/HomeFragment.kt
@@ -1,12 +1,19 @@
 package com.example.hackfestapp.ui.homefragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.hackfestapp.R
+import com.example.hackfestapp.databinding.FragmentHomeBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.DefaultPlayerUiController
+
 
 class HomeFragment : Fragment() {
 
@@ -26,7 +33,7 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
 }
