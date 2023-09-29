71c8d0e3938345bbef44b927a3c2fea57da9b3a1
G-Bhargav <okbhargav2004@gmail.com>
2023-09-30 02:59:46 +0530

    add: layouts of event Activity

---
+++ app/src/main/java/com/iitism/concetto/ui/allevents/ViewerActivity.kt
@@ -0,0 +1,15 @@
+package com.iitism.concetto.ui.allevents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iitism.concetto.R
+
+class ViewerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewer)
+
+
+
+    }
+}
