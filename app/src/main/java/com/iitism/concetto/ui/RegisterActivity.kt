68a607c94ef00ce4682488816922e65528f3c50b
aryan <aryanrdat@gmail.com>
2023-10-02 10:53:01 +0530

    rules adapter

---
+++ app/src/main/java/com/iitism/concetto/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.iitism.concetto.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iitism.concetto.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
