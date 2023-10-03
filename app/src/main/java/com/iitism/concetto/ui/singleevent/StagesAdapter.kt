03905036c0efa32eaa507ae5608c466e91fba0b3
G-Bhargav <okbhargav2004@gmail.com>
2023-10-03 15:34:55 +0530

    update: UI Improvements

--- app/src/main/java/com/iitism/concetto/ui/singleevent/StagesAdapter.kt
+++ app/src/main/java/com/iitism/concetto/ui/singleevent/StagesAdapter.kt
@@ -24,6 +24,7 @@ class StagesAdapter(private  val StagesArrayPair: ArrayList<Pair<String,Pair<Str
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentItem = StagesArrayPair[position]
+        holder.stageNo.text = "Stage ${position+1} :"
         holder.venueStage.text = currentItem.second.second
         holder.timing.text= currentItem.second.first
         holder.description.text = currentItem.first
@@ -31,6 +32,7 @@ class StagesAdapter(private  val StagesArrayPair: ArrayList<Pair<String,Pair<Str
     }
 
     class MyViewHolder(View : View) : RecyclerView.ViewHolder(View){
+        val stageNo : TextView = View.findViewById(R.id.tv_stageNo)
         val description : TextView = View.findViewById(R.id.tv_StageDescription)
         val timing : TextView = View.findViewById(R.id.tv_stageTiming)
         val venueStage : TextView = View.findViewById(R.id.tv_StageVenue)
