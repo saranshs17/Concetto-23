3ad69bcf453c25f03989950208eeef5d8965b48b
G-Bhargav <okbhargav2004@gmail.com>
2023-10-07 23:43:34 +0530

    add : AddNoticeFragment UI

---
+++ app/src/main/java/com/iitism/concetto/ui/noticeboardfragment/AddNoticeFragment.kt
@@ -0,0 +1,32 @@
+package com.iitism.concetto.ui.noticeboardfragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.iitism.concetto.R
+
+class AddNoticeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AddNoticeFragment()
+    }
+
+    private lateinit var viewModel: AddNoticeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_notice, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AddNoticeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/iitism/concetto/ui/noticeboardfragment/AddNoticeViewModel.kt
@@ -0,0 +1,7 @@
+package com.iitism.concetto.ui.noticeboardfragment
+
+import androidx.lifecycle.ViewModel
+
+class AddNoticeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
