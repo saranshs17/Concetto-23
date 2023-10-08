d9f804abbf231a756fb4edc7a4c87485dfc990ca
G-Bhargav <okbhargav2004@gmail.com>
2023-10-08 21:18:43 +0530

    update : AddNoticeFragment UI

--- app/src/main/java/com/iitism/concetto/ui/noticeboardfragment/AddNoticeFragment.kt
+++ app/src/main/java/com/iitism/concetto/ui/noticeboardfragment/AddNoticeFragment.kt
@@ -1,8 +1,10 @@
 package com.iitism.concetto.ui.noticeboardfragment
 
 import android.app.ProgressDialog
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,10 +12,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import com.iitism.concetto.R
 import com.iitism.concetto.databinding.FragmentAddNoticeBinding
 import com.iitism.concetto.ui.fcm_service_package.fcm_api_service_package.NotificationService
 import com.iitism.concetto.ui.fcm_service_package.token_api_service_package.ApiService
+import com.iitism.concetto.ui.homefragment.HomeFragment
 import com.iitism.concetto.ui.noticeboardfragment.retrofit.AddNoticeDataBody
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -39,21 +43,34 @@ class AddNoticeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(AddNoticeViewModel::class.java)
+
+
         binding.btnSendNotice.setOnClickListener {
             val title=binding.tvTitle.text.toString()
             val message=binding.tvMessage.text.toString()
-            val databody=AddNoticeDataBody(title,message)
-            viewModel.addNoticeService(databody)
-            Toast.makeText(binding.root.context,"Notice Sent Successfully",Toast.LENGTH_SHORT).show()
-            GlobalScope.launch {
-                tokenList = ApiService().getRegisteredTokenListService(requireContext())
-                delay(2500)
-                Log.d("Devices Token List=>>>", tokenList.toString())
-                delay(2500)
-                NotificationService().sendNotification(tokenList,message,title)
+            if(title.isEmpty()) binding.tvTitle.error= "title Can't be empty"
+            if(message.isEmpty()) binding.tvMessage.error = "Message can't be empty"
+            if(title.isNotEmpty() && message.isNotEmpty()){
+                val databody=AddNoticeDataBody(title,message)
+                viewModel.addNoticeService(databody)
+                Toast.makeText(binding.root.context,"Notice Sent Successfully",Toast.LENGTH_SHORT).show()
+                GlobalScope.launch {
+                    tokenList = ApiService().getRegisteredTokenListService(requireContext())
+                    delay(2500)
+                    Log.d("Devices Token List=>>>", tokenList.toString())
+                    delay(2500)
+                    NotificationService().sendNotification(tokenList,message,title)
+                }
+                binding.tvTitle.text?.clear()
+                binding.tvMessage.text?.clear()
+                binding.btnSendNotice.visibility = View.GONE
+                Handler().postDelayed({
+                    binding.btnSendNotice.visibility= View.VISIBLE
+                }, 2000)
             }
-            binding.tvTitle.text.clear()
-            binding.tvMessage.text.clear()
+
+
+
         }
     }
 
